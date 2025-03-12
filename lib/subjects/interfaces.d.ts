0ad2b02c1746656550c3831ff9a78b5b642fc566
ichou bahba <ichou.bahba@luxtrust.lu>
2025-03-12 13:40:22 +0000

    Add aliases service

--- lib/subjects/interfaces.d.ts
+++
@@ -1,9 +0,0 @@
-export interface SubjectDto {
-    id: string;
-    name: string;
-    emailAddress: string;
-    creationTimestamp: number;
-    defaultLanguage: string;
-    supersededBy: string;
-    superseded: string[];
-}
--- lib/tokens/api.d.ts
+++
@@ -1,4 +0,0 @@
-import { Observable } from 'rxjs';
-import { AccessToken } from './interfaces';
-import { RCApiOptions } from '../http';
-export declare function createToken(accessToken: AccessToken, options?: RCApiOptions): Observable<string>;
