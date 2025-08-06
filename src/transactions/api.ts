2d7664e7c7beb72351e72d79bde2209db4869429
[LT] Ichou Bahba <ichou.bahba@luxtrust.lu>
2025-08-06 17:13:34 +0200

    Fsdev 2568 transactions
    * 2.0.23
    
    * transactions

--- src/transactions/api.ts
+++ src/transactions/api.ts
@@ -2,10 +2,11 @@ import { Observable } from "rxjs";
 import { ConsentContext } from "../consents/interfaces";
 import { RightConsents } from "../api";
 import { TransactionDto, TransactionFilter } from "./interfaces";
+import { CollectionPage } from "../common";
 
 
-export function listTransactions(filter: TransactionFilter): Observable<TransactionDto[]> {
-  return RightConsents.http<TransactionDto[]>({
+export function listTransactions(filter: TransactionFilter): Observable<CollectionPage<TransactionDto[]>> {
+  return RightConsents.http<CollectionPage<TransactionDto[]>>({
     method: 'GET',
     url: `${RightConsents.config.apiRoot}/transactions`,
     headers: {
